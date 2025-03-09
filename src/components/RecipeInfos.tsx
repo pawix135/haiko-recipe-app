eb9e1d4a9cdfdf0eadf8182801fef182eeb19dcf
pawix135 <leagueoflegendsoppl@gmail.com>
2025-03-09 01:08:32 +0100

    FIXED: Trash button on the thumbnail in correct place

--- src/components/RecipeInfos.tsx
+++ src/components/RecipeInfos.tsx
@@ -1,43 +1,65 @@
-import { useFormContext } from "react-hook-form"
-import { FormControl, FormField, FormItem, FormLabel, FormMessage } from "./ui/form"
-import { Input } from "./ui/input"
-import { Recipe } from "../types/recipe"
-import { RECIPE_INFOS_AMOUNT_LABEL, RECIPE_INFOS_AMOUNT_PLACEHOLDER, RECIPE_INFOS_NAME_LABEL, RECIPE_INFOS_NAME_PLACEHOLDER, RECIPE_THUMBNAIL_CALL_TO_ACTION, RECIPES_INFO_PIECES_KIND_PLACEHOLDER } from "../constants/text"
-import { Label } from "./ui/label"
-import { ThumbnailImage } from "./ThumbnailImage"
-import { Trash } from "lucide-react"
-import { Button } from "./ui/button"
+import { useFormContext } from "react-hook-form";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { Input } from "./ui/input";
+import { Recipe } from "../types/recipe";
+import {
+  RECIPE_INFOS_AMOUNT_LABEL,
+  RECIPE_INFOS_AMOUNT_PLACEHOLDER,
+  RECIPE_INFOS_NAME_LABEL,
+  RECIPE_INFOS_NAME_PLACEHOLDER,
+  RECIPE_THUMBNAIL_CALL_TO_ACTION,
+  RECIPES_INFO_PIECES_KIND_PLACEHOLDER,
+} from "../constants/text";
+import { Label } from "./ui/label";
+import { ThumbnailImage } from "./ThumbnailImage";
+import { Trash } from "lucide-react";
+import { Button } from "./ui/button";
 
-interface Props {
-}
+interface Props {}
 
 export const RecipeInfos: React.FC<Props> = () => {
-
   const { control, register, ...form } = useFormContext<Recipe>();
 
-  let formData = form.watch()
+  let formData = form.watch();
 
   const removeThumbnail = () => {
-    form.setValue('thumb.nail', '');
-  }
+    form.setValue("thumb.nail", "");
+  };
 
   return (
-    <div className="flex flex-row space-y-4 h-[150px]">
-      <div className='relative flex flex-row space-x-5'>
+    <div className="flex flex-row space-y-4 space-x-5 h-[150px]">
+      <div className="relative flex flex-row size-[150px]">
         {formData.thumb.nail ? (
           <div className="size-[150px]">
-            <img src={"data:image/jpeg;base64," + formData.thumb.nail} alt="Vorschaubild" className="w-[150px] rounded-[10px]" />
-            <Button type="button" variant={"destructive"} onClick={removeThumbnail} className="absolute right-0 bottom-0 z-10">
+            <img
+              src={"data:image/jpeg;base64," + formData.thumb.nail}
+              alt="Vorschaubild"
+              className="size-[150px] rounded-[10px]"
+            />
+            <Button
+              type="button"
+              variant={"destructive"}
+              onClick={removeThumbnail}
+              className="absolute right-0 bottom-0 z-10"
+            >
               <Trash />
             </Button>
           </div>
-        ) :
-          <Label htmlFor="thumbnail" className='flex flex-col size-[150px]'>
+        ) : (
+          <Label htmlFor="thumbnail" className="flex flex-col size-[150px]">
             <div className="border border-accent-foregroundt w-full h-[150px] relative grid place-items-center rounded-[10px]">
-              <span className="text-center">{RECIPE_THUMBNAIL_CALL_TO_ACTION}</span>
+              <span className="text-center">
+                {RECIPE_THUMBNAIL_CALL_TO_ACTION}
+              </span>
             </div>
           </Label>
-        }
+        )}
         <ThumbnailImage />
       </div>
       <div className="w-full h-full space-y-2 flex flex-col justify-between">
@@ -46,22 +68,30 @@ export const RecipeInfos: React.FC<Props> = () => {
           name="infos.name"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>{RECIPE_INFOS_NAME_LABEL}* <FormMessage /></FormLabel>
+              <FormLabel>
+                {RECIPE_INFOS_NAME_LABEL}* <FormMessage />
+              </FormLabel>
               <FormControl>
                 <Input placeholder={RECIPE_INFOS_NAME_PLACEHOLDER} {...field} />
               </FormControl>
             </FormItem>
           )}
         />
-        <div className='flex flex-row space-x-2'>
+        <div className="flex flex-row space-x-2">
           <FormField
             control={control}
             name="infos.pieces"
             render={({ field }) => (
-              <FormItem className='w-half'>
-                <FormLabel>{RECIPE_INFOS_AMOUNT_LABEL}* <FormMessage /></FormLabel>
+              <FormItem className="w-half">
+                <FormLabel>
+                  {RECIPE_INFOS_AMOUNT_LABEL}* <FormMessage />
+                </FormLabel>
                 <FormControl>
-                  <Input type='number' placeholder={RECIPE_INFOS_AMOUNT_PLACEHOLDER} {...register(field.name, { valueAsNumber: true })} />
+                  <Input
+                    type="number"
+                    placeholder={RECIPE_INFOS_AMOUNT_PLACEHOLDER}
+                    {...register(field.name, { valueAsNumber: true })}
+                  />
                 </FormControl>
               </FormItem>
             )}
@@ -70,10 +100,16 @@ export const RecipeInfos: React.FC<Props> = () => {
             control={control}
             name="infos.pieces_kind"
             render={({ field }) => (
-              <FormItem className='w-full'>
-                <FormLabel>&nbsp;<FormMessage /></FormLabel>
+              <FormItem className="w-full">
+                <FormLabel>
+                  &nbsp;
+                  <FormMessage />
+                </FormLabel>
                 <FormControl>
-                  <Input placeholder={RECIPES_INFO_PIECES_KIND_PLACEHOLDER} {...field} />
+                  <Input
+                    placeholder={RECIPES_INFO_PIECES_KIND_PLACEHOLDER}
+                    {...field}
+                  />
                 </FormControl>
               </FormItem>
             )}
@@ -81,5 +117,5 @@ export const RecipeInfos: React.FC<Props> = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
